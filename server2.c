089629b2c82d7c5921b54fa6e4c66b44b7dcdd28
david <david.michael.audet@gmail.com>
2010-01-24 19:26:26 +0000

    added seq line in server2

--- server2.c
+++ server2.c
@@ -7,6 +7,7 @@
 #include<sys/types.h>
 #include<unistd.h>
 #include<string.h>
+#include<time.h>
 
 int main(int argc, char**argv){
 
@@ -34,12 +35,13 @@ int main(int argc, char**argv){
 	server.sin_port = htons(atoi(argv[1]));
 
 
-	if(((bind(sock, (struct sockaddr *)&server, sizeof(server))) == -1) || (listen(sock, 50) == -1)){
+	if(((bind(sock, (struct sockaddr *)&server, sizeof(server))) == -1) || (listen(sock, 10) == -1)){
 		printf("Could not bind or listen on port %d\n", atoi(argv[1]));
 		return 1;
 	}
 
-	printf("Waiting for data on port %d...\n", atoi(argv[1]));
+	printf("sws is running on TCP port %d and serving %s\n", atoi(argv[1]), argv[2]);
+	printf("press 'q' to quit ...\n");
 	
 	if((retval = accept(sock, (struct sockaddr *)&client, &length)) == -1){
 		printf("Could not accept client\n");
@@ -51,12 +53,17 @@ int main(int argc, char**argv){
 	char* greeting = "Thanks for the message!!\n";
 	char* reply = malloc(sizeof(char)*512);
 	int receivedMsgSize;
+	time_t time;
+	struct tm* tmstring;
+	int seq = 0;
+	char timestring[256];
 	send(retval, greeting, strlen(greeting), 0);
 	for(;;){	
-		if((receivedMsgSize = recv(retval, reply, 512, 0)) < 0)
+		if((receivedMsgSize = recv(retval, reply, 512, 0)) < 0){
 			printf("Error receiving message\n");
-		
-			printf("message from client: %s\n", reply);
+			tmstring = localtime(&time);
+			strftime(timestring, sizeof(timestring), "%y %b %d %T", tmstring);  
+			printf("Seq no. %d %s\n", ++seq, timestring);
 			send(retval, reply, strlen(greeting), 0);
 		}
 	}
